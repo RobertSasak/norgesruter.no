9c9d13f5d74b47fb9cee9bb18dc7b5efdf92d219
RobertSasak <robert.sasak@gmail.com>
2015-06-18 19:25:08 +0200

    Updated autocomplete.

--- app/js/directives/inputStop/inputStop.js
+++ app/js/directives/inputStop/inputStop.js
@@ -5,53 +5,34 @@ var angular = require('angular');
 /**
  * @ngInject
  */
-directivesModule.directive('inputStop', function () {
+directivesModule.directive('inputStop', function (ReiseInfo) {
 	return {
 		restrict: 'E',
 		templateUrl: 'inputStop/inputStop.html',
 		controller: function ($window) {
 			var vm = this;
 			var Bloodhound = $window.Bloodhound;
-
-			// Instantiate the bloodhound suggestion engine
-			var numbers = new Bloodhound({
-				datumTokenizer: function (d) {
-					return Bloodhound.tokenizers.whitespace(d.num);
-				},
-				queryTokenizer: Bloodhound.tokenizers.whitespace,
-				local: [{
-					num: 'one'
-				}, {
-					num: 'two'
-				}, {
-					num: 'three'
-				}, {
-					num: 'four'
-				}, {
-					num: 'five'
-				}, {
-					num: 'six'
-				}, {
-					num: 'seven'
-				}, {
-					num: 'eight'
-				}, {
-					num: 'nine'
-				}, {
-					num: 'ten'
-				}]
-			});
-
-			// initialize the bloodhound suggestion engine
-			numbers.initialize();
+			var authKey = 'api-test';
+			var baseUrl = 'http://crossorigin.me/http://hafas.websrv05.reiseinfo.no/bin/dev/nri/rest.exe/v1.1/vs_restapi/';
 
 			// Typeahead options object
 			vm.options = {
+				id: vm.id,
 				highlight: true,
-				editable: false
+				editable: false,
+				minLength: 0
 			};
 
-
+			// maximum 33000 for {id,name}
+			// maximum 34000 for {i,n}
+			function transformMinify(array) {
+				return array.slice(0, 34000).map(function (a) {
+					return {
+						id: a.id,
+						name: a.name
+					};
+				});
+			}
 
 			var remote = new Bloodhound({
 				datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
@@ -60,51 +41,70 @@ directivesModule.directive('inputStop', function () {
 					return obj.id;
 				},
 				remote: {
-					url: 'http://crossorigin.me/http://hafas.utvikling01.reiseinfo.no/bin/dev/nri/rest.exe/v1.1/vs_restapi/location?authKey=api-test&format=json&input=%QUERY',
+					url: baseUrl + 'location?authKey=' + authKey + '&format=json&input=%QUERY',
 					wildcard: '%QUERY',
 					transform: function (response) {
-						if (angular.isArray(response.LocationList.StopLocation)) {
-							return response.LocationList.StopLocation;
+						if (!response.LocationList.StopLocation) {
+							return [];
 						}
-						return [response.LocationList.StopLocation];
+						if (angular.isArray(response.LocationList.StopLocation)) {
+							return transformMinify(response.LocationList.StopLocation);
+						}
+						return transformMinify([response.LocationList.StopLocation]);
 					}
-				}
-			});
-
-
-			var local = new Bloodhound({
-				datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-				queryTokenizer: Bloodhound.tokenizers.whitespace,
-				identify: function (obj) {
-					return obj.id;
 				},
 				prefetch: {
 					url: '/data/allStops.json',
+					transform: transformMinify,
+					ttl: 14 * 24 * 60 * 60 * 1000
 				}
 			});
 
-			vm.datasets = [{
-				displayKey: 'num',
-				source: numbers.ttAdapter(),
-				templates: {
-					header: 'Numbers'
+			remote.initialize();
+
+			function nearBy(q, sync, async) {
+				if (q === '') {
+					ReiseInfo.locationNearByStops({
+						originCoordLat: 60.391263,
+						originCoordLong: 5.322054
+					}).then(function (response) {
+						return response.LocationList.StopLocation;
+					}).then(function (data) {
+						async(data);
+					});
+					//TODO: remove
+					//var a = local.get([6653]);
+					//sync(a);
+				} else {
+					sync();
+					async();
 				}
-			}, {
+			}
+
+			vm.datasets = [{
 				displayKey: 'name',
+				//limit: 50,
 				templates: {
 					header: 'Local',
 				},
-				source: local
+				source: remote
 			}, {
 				displayKey: 'name',
 				templates: {
-					header: 'Remote',
+					header: 'Near by',
+					notFound: 'No stops near by',
+					pending: 'Searching nearby stops...'
 				},
-				source: remote
+				source: nearBy
 			}];
 		},
 		controllerAs: 'ctrl',
-		bindToController: {},
-		scope: {},
+		bindToController: {
+			id: '@',
+			model: '=',
+			placeholder: '@',
+			initialValue: '='
+		},
+		scope: {}
 	};
 });
