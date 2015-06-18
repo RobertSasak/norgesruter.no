300e8f92d779919ec6370884aa2914e52a4a1203
RobertSasak <robert.sasak@gmail.com>
2015-06-18 15:55:51 +0200

    Focus service.

---
+++ app/js/services/focus.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var servicesModule = require('./_index.js');
+var angular = require('angular');
+
+/**
+ * @ngInject
+ */
+servicesModule.factory('Focus', function ($timeout, $window) {
+    return function (id) {
+        // timeout makes sure that it is invoked after any other event has been triggered.
+        // e.g. click events that need to run before the focus or
+        // inputs elements that are in a disabled state but are enabled when those events
+        // are triggered.
+        $timeout(function () {
+            var element = $window.document.getElementById(id);
+            if (element) {
+                element.focus();
+            }
+        });
+    };
+});
