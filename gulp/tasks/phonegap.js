c7d08a8a969a9cdcdfd374c5b40e0bbd50638e78
RobertSasak <robert.sasak@gmail.com>
2015-07-06 17:45:02 +0200

    Better phonegap build.

--- gulp/tasks/phonegap.js
+++ gulp/tasks/phonegap.js
@@ -2,19 +2,38 @@
 
 var gulp = require('gulp');
 var phonegapBuild = require('gulp-phonegap-build');
+var insertLines = require('gulp-insert-lines');
 var config = require('../../.phonegap.json');
 var version = require('../../package.json').version;
-var pathWithoutExt = 'release/norgesruter' + version;
+var pathWithoutExt = 'release/norgesruter.' + version;
+var replace = require('gulp-replace');
+
+gulp.task('templates', function () {
+    gulp.src(['file.txt'])
+        .pipe(gulp.dest('build/file.txt'));
+});
 
 var files = ['build/**/*', 'config.xml', 'resource*/**/*.png'];
 
-gulp.task('phonegap-www', ['prod'], function () {
-    gulp.src(files)
-        .pipe(gulp.dest('./phonegap'));
+
+gulp.task('insert-cordova-js', ['prod'], function () {
+    gulp.src('./build/index.html')
+        .pipe(insertLines({
+            'before': /<\/head>$/,
+            'lineBefore': '<script src="cordova.js"></script>'
+        }))
+        .pipe(gulp.dest('build'));
 });
 
-gulp.task('phonegap-build', ['prod'], function () {
+gulp.task('phonegap-www', ['insert-cordova-js'], function () {
     gulp.src(files)
+        .pipe(replace('versionFromPackageJson', version))
+        .pipe(gulp.dest('./www'));
+});
+
+gulp.task('phonegap-build', ['insert-cordova-js'], function () {
+    gulp.src(files)
+        .pipe(replace('versionFromPackageJson', version))
         .pipe(phonegapBuild({
             timeout: 2000000,
             download: {
