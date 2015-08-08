51cfcaa9aa0ef2e393f5fc6fdc49094f880fbbe1
RobertSasak <robert.sasak@gmail.com>
2015-08-08 21:28:29 +0200

    Nicer urls

--- app/js/on_config.js
+++ app/js/on_config.js
@@ -1,22 +1,53 @@
 'use strict';
 
+var angular = require('angular');
 /**
  * @ngInject
  */
-function OnConfig($stateProvider, $locationProvider, $urlRouterProvider) {
+function OnConfig($stateProvider, $locationProvider, $urlRouterProvider, $urlMatcherFactoryProvider) {
 
 	//HTML5 mode does not work on ghpages
 	//$locationProvider.html5Mode(true);
 
+	$urlMatcherFactoryProvider.type('stopName', {
+		encode: function (str) {
+			return str && str.replace(/ /g, '-').replace('/', '');
+		},
+		decode: function (str) {
+			console.log(str);
+			return str && str.replace(/-/g, ' ');
+		},
+		is: angular.isString,
+		pattern: /[^/]+/
+	});
+
 	$stateProvider
 		.state('trip', {
-			url: '/trip?originId&originName&destId&destName&time&date',
+			url: '/t/{originName:stopName}/{originId:[0-9]+}/{destName:stopName}/{destId:[0-9]+}?time&date',
+			params: {
+				originName: {
+					value: null,
+					squash: true
+				},
+				originId: {
+					value: null,
+					squash: true
+				},
+				destName: {
+					value: null,
+					squash: true
+				},
+				destId: {
+					value: null,
+					squash: true
+				}
+			},
 			templateUrl: 'trip.html',
 			controller: 'Trip',
 			controllerAs: 'ctrl'
 		});
 
-	$urlRouterProvider.otherwise('/trip');
+	$urlRouterProvider.otherwise('/t');
 
 }
 
